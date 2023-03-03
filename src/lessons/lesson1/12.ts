2a7840dd37b31d8331bdb04814288507e97206e1
lebra87 <lebra@bk.ru>
2023-03-03 22:08:28 +0300

    Задание 12 - Добавила комментарии при выводе чисел

--- src/lessons/lesson1/12.ts
+++ src/lessons/lesson1/12.ts
@@ -1,9 +1,11 @@
-for (let number: number = 0; number <= 10; number++) {
+console.log('Четные числа:')
+for (let number: number = 0; number <= 100; number++) {
     if (number % 2 == 0) {
         console.log(number)
     }
 }
-for (let number: number = 0; number <= 10; number++) {
+console.log('Нечетные числа:')
+for (let number: number = 0; number <= 100; number++) {
     if (number % 2 != 0) {
         console.log(number)
     }
